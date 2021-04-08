5b7d44e17aaa10447fe2224164f31b6b4e47816c
Joao Pereira <ajpereira88@gmail.com>
2021-04-08 16:24:33 -0300

    Atualização 01 - aula 07/04/2021

--- PRÁT. PROF. I - SISTEMAS COMPUTACIONAIS/PYTHON/testemudanca.py
+++ PRÁT. PROF. I - SISTEMAS COMPUTACIONAIS/PYTHON/testemudanca.py
@@ -41,18 +41,11 @@ class Binario:
                     algoritmo[i] = hexa[valores]
             i += 1
 
-
-
     def calcula_binario_int(self, valor, base):
+        self.__valor = valor
 
-        print("PARTE INTEIRA:\n")
-        self.__valor = valor 
         self.__base = int(base)
         self.__algoritmo = []
-        self.__valor_raw = self.__valor.split(',')
-        
-        self.__valor = int(self.__valor_raw[0])
-        self.__tamanho = len(self.__valor_raw)
 
         while self.__valor > 0:
             resto = self.__valor % self.__base
@@ -67,64 +60,7 @@ class Binario:
             self.__hexadecimal(self.__algoritmo)
 
         print(self.__algoritmo)
-        print("")
 
-        if self.__tamanho > 1:
-            self.__valor_fracionario = float("0." + self.__valor_raw[1])
-            self.calcula_binario_fracionario(self.__base)
-
-        self.imprime_resultado()
-        self.apagalistas()
-
-    def calcula_binario_fracionario(self, base):
-        print("PARTE FRACIONÁRIA:\n")
-        n=0
-        self.__parte_fracionaria =[]
-        while (self.__valor_fracionario != 0 and n < 8):
-
-            self.__intermediario = self.__valor_fracionario * base
-
-            if self.__intermediario > 0:
-                self.__inteiro = int(self.__intermediario)
-                self.__parte_fracionaria.append(self.__inteiro)
-                print("{:.5f}".format(self.__valor_fracionario, 4), f" * {base} =",
-                      "{:.5f}".format(self.__intermediario),
-                      f"-> {self.__inteiro}")
-
-                self.__valor_fracionario = self.__intermediario - self.__inteiro
-            else:
-                self.__parte_fracionaria.append(0)
-                print("{:.5f}".format(self.__valor_fracionario, 4), f" * {base} =",
-                      "{:.5f}".format(self.__intermediario),
-                      f"-> {0}")
-            n += 1
-
-        if self.__base == 16:
-            self.__hexadecimal(self.__parte_fracionaria)
-
-
-        print(f"{self.__parte_fracionaria}\n")
-
-
-    def imprime_resultado(self):
-        if self.__base == 2:
-            print("O VALOR EQUIVALENTE EM BINÁRIO É:\n")
-        elif self.__base == 16:
-            print("O VALOR EQUIVALENTE EM HEXADECIMAL É:\n")
-
-        for i in self.__algoritmo:
-            print(i, end="")
-
-        if self.__tamanho > 1:
-            print(",", end="")
-            for i in self.__parte_fracionaria:
-                print(i, end="")
-        print("\n")
-
-    def apagalistas(self):
-        self.__algoritmo.clear()
-        if self.__tamanho > 1:
-            self.__parte_fracionaria.clear()
 
 class Decimal:
     """
